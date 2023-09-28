df0f46ec36331dbb64f6926f6131f21ff17216ee
SanketKumarDev <141023706+SanketKumarDev@users.noreply.github.com>
2023-09-28 23:22:49 +0530

    Add files via upload

---
+++ script/common.js
@@ -0,0 +1,141 @@
+const Common = (function () {
+    const apiToken = '2395742737283215';
+    const apiUrl = `https://www.superheroapi.com/api.php/${apiToken}/`;
+    const toastContainer = document.getElementById('toast');
+    const FAVOURITES = 'favourites';
+    const loader = document.querySelector('.loader');
+
+    function setRandomBackgroundImage() {
+        const urls = [
+            'https://wallpaperaccess.com/full/1225711.jpg',
+            'https://wallpaperaccess.com/full/1225850.jpg',
+            'https://tpc.googlesyndication.com/simgad/9448295044806331629?sqp=4sqPyQQ7QjkqNxABHQAAtEIgASgBMAk4A0DwkwlYAWBfcAKAAQGIAQGdAQAAgD-oAQGwAYCt4gS4AV_FAS2ynT4&rs=AOga4qmJFA45Vj8BCurnWRVt1k1tZyuF_g',
+            'https://wallpaperaccess.com/full/94681.jpg',
+            
+        ];
+
+        const randomBackgroundImageUrl =
+            urls[Math.floor(Math.random() * urls.length)];
+
+       
+        const html = document.querySelector('html');
+        html.style.backgroundImage = `url(${randomBackgroundImageUrl})`;
+    }
+
+    function showLoader() {
+        loader.style.display = 'block';
+    }
+
+    function hideLoader() {
+        loader.style.display = 'none';
+    }
+
+    /* Notification handler */
+    function showNotification(type, message) {
+        if (type === 'error') {
+            toastContainer.classList.remove('toast-success');
+            toastContainer.classList.add('toast-error');
+        } else if (type === 'success') {
+            toastContainer.classList.remove('toast-error');
+            toastContainer.classList.add('toast-success');
+        }
+        toastContainer.style.display = 'block';
+        toastContainer.innerText = message;
+
+        setTimeout(() => {
+            toastContainer.style.display = 'none';
+        }, 3000);
+    }
+
+    /* Send api requests */
+    async function apiRequest(url) {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+
+            return {
+                data,
+                success: true,
+            };
+        } catch (error) {
+            console.log('error', error);
+            return {
+                error: error.message,
+                success: false,
+            };
+        }
+    }
+
+    /* Add hero to localstorage */
+    function addHeroToFavourites(hero) {
+        if (!hero) return;
+
+        const favouritesFromLocalStorage = getFavouriteSuperheroes();
+        favouritesFromLocalStorage.push(hero);
+
+        // Save in localstorage
+        localStorage.setItem(
+            FAVOURITES,
+            JSON.stringify(favouritesFromLocalStorage)
+        );
+
+        showNotification('success', 'Added to favourites');
+    }
+
+    /* Remove hero from localstorage */
+    function removeHeroFromFavourites(heroId) {
+        if (!heroId) return;
+
+        let favouritesFromLocalStorage = getFavouriteSuperheroes();
+
+        // Remove hero from localstorage
+        favouritesFromLocalStorage = favouritesFromLocalStorage.filter(
+            (item) => item.id !== heroId
+        );
+
+        // Save in localstorage
+        localStorage.setItem(
+            FAVOURITES,
+            JSON.stringify(favouritesFromLocalStorage)
+        );
+
+        showNotification('success', 'Removed from favourites');
+    }
+
+    /* Get fav superheroes from the local storage */
+    function getFavouriteSuperheroes() {
+        return localStorage.getItem(FAVOURITES) ?
+            JSON.parse(localStorage.getItem(FAVOURITES)) :
+            [];
+    }
+
+    function debounce(func, delay) {
+        let timeout;
+        return function () {
+            const context = this;
+            const args = arguments;
+
+            clearTimeout(timeout);
+
+            timeout = setTimeout(function () {
+                timeout = null;
+                func.apply(context, args);
+                // handleSearch(args);
+            }, delay);
+        };
+    }
+
+    setRandomBackgroundImage();
+
+    return {
+        apiRequest,
+        apiUrl,
+        showNotification,
+        addHeroToFavourites,
+        removeHeroFromFavourites,
+        getFavouriteSuperheroes,
+        showLoader,
+        hideLoader,
+        debounce
+    };
+})();
